0e01088d5a9b54f1ceaf9c0b9d3cccd2e705963f
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2020-06-21 20:53:10 -0300

    feat: Adding demo project

---
+++ src/main/java/test/demo/App.java
@@ -0,0 +1,12 @@
+package test.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println( "\nHello World! This is a demo project\n" );
+    }
+}
